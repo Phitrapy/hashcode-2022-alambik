7a064f81cacf998bd7da9043dc5ee76e2a3a3d1f
Phitrapy <clement.begnaud@gmail.com>
2022-02-24 19:40:49 +0100

    LEZGO

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
-// Import stylesheets
-import './style.css';
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+// Code exemple
+const nom: string = `crevard`; // const -> initialise une constante
+let oups = 2; // let -> initialise une variable
+
+function faisCoucou(nomPersonne?: string) {
+    console.log(`Coucou`, nomPersonne, `!`);
+}
+
+faisCoucou(nom);
