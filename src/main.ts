d8f568c9964690782c48aa1e1b7f3a4f3ad27447
Phitrapy <clement.begnaud@gmail.com>
2022-02-24 20:13:55 +0100

    ouvre le fichier dont le nom est donné en argument

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import * as fs from 'fs';
+
 export function main(nomFichier: string) { // C'est ici qu'on exécute le code
     console.log("on est dans le main");
     // 1) Lire un fichier en input
@@ -10,4 +12,10 @@ export function main(nomFichier: string) { // C'est ici qu'on exécute le code
 
 function lireFichier(nomFichier: string) {
     console.log(`Fichier à ouvrir:`, nomFichier);
+    const file = fs.readFileSync(nomFichier,'utf8');
+    console.log(file);
+}
+
+function ecrireFichier(nomFichier: string) {
+
 }
