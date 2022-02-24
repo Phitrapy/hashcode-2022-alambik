172e8d44b39b785f3e8aa1d2cdb72ffe267e7c1f
Phitrapy <clement.begnaud@gmail.com>
2022-02-24 19:52:11 +0100

    Donne les étapes dans le main! (et autres)

--- index.ts
+++ index.ts
@@ -1,9 +1,17 @@
 // Lancer le code avec:
+// npm install
 // npx ts-node index.ts
 
 import { main } from "./src/main";
 
-main(); // Placer le code à écécuter dans la méthode main.
+// récupère le nom du fichier en input:
+const nomFichier = process.argv[2];
+
+if (nomFichier){ 
+    main(nomFichier); // Placer le code à écécuter dans la méthode main.
+} else {
+    console.error(`TUTUTUT! Il faut fournir le nom de l'input en argument!`)!
+}
 
 
 // Code exemple
@@ -14,6 +22,5 @@ function faisCoucou(nomPersonne?: string) {
     console.log(`Coucou`, nomPersonne, `!`);
 }
 
-faisCoucou(nom);
-
-
+// décommenter pour exécuter:
+// faisCoucou(nom);
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,13 @@
-export function main() { // C'est ici qu'on exécute le code
+export function main(nomFichier: string) { // C'est ici qu'on exécute le code
     console.log("on est dans le main");
+    // 1) Lire un fichier en input
+    lireFichier(nomFichier);
+
+    // 2) Faire la tambouille
+
+    // 3) Ecrire un fichier en output
+}
+
+function lireFichier(nomFichier: string) {
+    console.log(`Fichier à ouvrir:`, nomFichier);
 }
