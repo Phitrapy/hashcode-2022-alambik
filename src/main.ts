87890dc9c0a57c82d9955c6432981fdaf339a3c5
Clement BEGNAUD <clement.begnaud-ext@rca.fr>
2022-04-04 13:32:44 +0200

    allez on s'accroche

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import * as fs from 'fs';
+import { removeAllListeners } from 'process';
+import Contributor from './model/contributor.model';
+import Project from './model/project.model';
+import { Situation } from './model/situation.model';
+import Skill from './model/skill.model';
 import { readInputFile } from "./read-input-file";
 
 export function main(nomFichier: string) { // C'est ici qu'on exécute le code
@@ -8,7 +13,7 @@ export function main(nomFichier: string) { // C'est ici qu'on exécute le code
     const situation = readInputFile(fichier);
 
     // 2) Faire la tambouille
-    tambouiller();
+    tambouiller(situation);
 
     // 3) Ecrire un fichier en output
     ecrireFichier(nomFichier, `test écriture`);
@@ -30,8 +35,18 @@ function ecrireFichier(nomFichier: string, contenu: any) {
     });
 }
 
-function tambouiller() {
-    
+function tambouiller({contributors, projects}: Situation) {
+
+    const kanban: Kanban = new Kanban({freeContributors: contributors, todo: projects});
+    const history = [kanban];
+
+    // Un jour... S'il reste des projets à faire
+    while (!kanban.isDone) {
+        // Mettre à jour le kanban
+        kanban.update();
+        
+    }
+
     /*
         Stratégies:
         1) task killer
@@ -41,4 +56,110 @@ function tambouiller() {
 
         Mesures
     */
+}
+
+class Kanban {
+    freeContributors: Contributor[] = [];
+    todo: Project[] = [];
+    doing: Project[] = [];
+    done: Project[] = [];
+    stale: Project[] = [];
+
+    private _day = 0;
+
+    public get day(): number {
+        return this.day;
+    }
+
+    public get isDone() {
+        return this.flatRaf.length === 0;
+    }
+
+    public get raf(): {todo: Project[], doing: Project[]} {
+        return {todo: this.todo, doing: this.doing};
+    }
+
+    public get flatRaf(): Project[] {
+        return this.raf.todo.concat(this.raf.doing);
+    }
+
+    public get rafPotentialWorth(): number{
+        return this.flatRaf.reduce((w,p) => w + p.potentialWorth(this.day), 0);
+    }
+
+    public get worth(): number {
+        return this.done.reduce((w,p) => w + p.worth(this.day), 0);
+    }
+
+    constructor(kanban: Partial<Kanban>) {
+        Object.keys(kanban).forEach(key => this[key] = kanban[key]);
+    }
+
+    /**
+     * Met à jour le Kanban
+     */
+    update() {
+        // Vérifier l'avancement
+            // Marquer les projets finis de doing à done, et les projets sans valeur de doing à stale
+        const newDoing: Project[] = this.doing.reduce((list, p) => !p.isDone? [...list, p] : list, []);
+        const newDone: Project[] = this.doing.reduce((list, p) => p.isDone? [...list, p] : list, []);
+            // Libérer les gens affectés aux projets finis ou stale
+        this.freeContributors.concat(newDone.flatMap(p => p.contributors)).filter((p1, i1, arr) => arr.indexOf(p1) === i1);
+        this.doing = newDoing;
+        this.done = [...this.done, ...newDone];
+            // Passer les projets todo et doing sans valeur potentielle de todo à stale
+        const newStale: Project[] = this.todo.reduce((list, p) => p.potentialWorth(this.day) === 0 ? [...list, p] : list, []);
+        newStale.concat(this.doing.reduce((list, p) => p.potentialWorth(this.day) === 0 ? [...list, p] : list, []));
+        this.stale = [...this.stale, ...newStale];
+    }
+
+    /**
+     * Attribue des gens à des projets
+     */
+    affect() {
+        let freeContributors = [...this.freeContributors];
+        const markedForDoingIndex: number[] = [];
+        this.todo.forEach((p, index) => {
+            try {
+                const {applicants, remaining} = findApplicants(p, freeContributors);
+                p.contributors = applicants;
+                freeContributors = remaining;
+                markedForDoingIndex.push(index);
+            } catch (error) {
+                console.error(error);
+            }
+        });
+        
+        // Marquer les projets en cours de todo à doing
+        const newTodo: Project[] = this.todo.reduce((list, p) => p.contributors.length !== 0 ? [...list, p] : list, []);
+        const newDoing: Project[] = this.todo.reduce((list, p) => p.contributors.length === 0? [...list, p] : list, []);
+    }
+}
+
+function findApplicants(p: Project, contributors: Contributor[]): {applicants: Contributor[], remaining: Contributor[]} {
+    const remaining = [...contributors];
+    const applicants: Contributor[] = [];
+
+    const needMentor: Skill[] = [];
+
+    // Essaie d'affecter un contributeur à chaque rôle
+    p.skills.forEach(projectSkill => {
+        const applicantIndex = remaining.findIndex((c) => c.skills.some(contribSkill => contribSkill.nom === projectSkill.nom && contribSkill.niveau >= projectSkill.niveau));
+        
+        if (applicantIndex === -1) {
+            needMentor.push(projectSkill);
+        } else {
+            applicants.push(...remaining.splice(applicantIndex, 1));
+        }
+    });
+
+    // Essaie d'affecter un contributeur mentoré à chaque rôle non pourvu
+    // const unmatchedSkill = needMentor.reduce((list, p) => {
+    //     const potentialApplicants = remaining.findIndex((c) => c.skills.some(contribSkill => contribSkill.nom === projectSkill.nom && contribSkill.niveau >= projectSkill.niveau));
+    //     [...p, ]
+    // }, []);
+
+    if (applicants.length < p.skills.length) throw `Pas assez de contributeurs qualifiés sur le projet ${p.nom}(manque: ${p.skills.length - applicants.length}))`;
+
+    return {applicants, remaining};
 }
--- src/model/project.model.ts
+++ src/model/project.model.ts
@@ -1,12 +1,51 @@
 import Contributor from "./contributor.model";
 import Skill from "./skill.model";
 
-export default interface Project {
-    nom: string,
-    skills: Skill[],
-    duree: number,
-    bestBefore: number,
-    score: number
+export default class Project {
+    nom: string;
+    skills: Skill[];
+    duree: number;
+    progress: number;
+    dayDone?: number
+    bestBefore: number;
+    score: number;
 
-    contributors: Contributor[] // pas sûr
+    contributors: Contributor[] = [] // pas sûr
+
+    get isDone(): boolean {
+        return this.remainingDays === 0;
+    }
+
+    get remainingDays(): number{
+        return this.duree - this.progress;
+    }
+
+    public worthDecay(day: number): number {
+        const decay = this.bestBefore - day;
+        return Math.min(decay, 0);
+    }
+
+    public potentialWorhDecay(day: number): number {
+        const decay = this.bestBefore - (day + this.remainingDays);
+        return Math.min(decay, 0);
+    }
+
+    public worth(day: number) {
+        if (!this.isDone) {
+            return 0;
+        }
+        return Math.max(this.score - this.worthDecay(day), 0);
+    }
+
+    public potentialWorth(day: number) {
+        return Math.max(this.score - this.potentialWorhDecay(day), 0);
+    }
+
+    public work() {
+        if (!this.isDone) this.progress++;
+    }
+
+    public done(day: number) {
+        if(!this.isDone) this.dayDone = day;
+    }
 }
